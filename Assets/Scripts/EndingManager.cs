42859ce7b33b6d6d9d85213c6a2b0e08bf7d2e92
Zezhong Zhang <zezhongz@uci.edu>
2021-02-01 03:34:23 +0800

    Ending Scene

---
+++ Assets/Scripts/EndingManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingManager : MonoBehaviour
+{
+    public void backToMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
