692b3b89ec69b45b8a56be362f3e616f325e8efb
Zezhong Zhang <zezhongz@uci.edu>
2021-02-01 01:24:41 +0800

    Gameover Scene

---
+++ Assets/Scripts/GameoverManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameoverManager : MonoBehaviour
+{
+    public void loadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void backToMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
